976117d51091cedff5d867cbaae20c90abbe7e18
DavidTrstenjak <90246263+DavidTrstenjak@users.noreply.github.com>
2022-10-16 18:43:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool prim = false;
+            int primBroj = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+
+                for (int j = 2; j <= 100; j++)
+                {
+                    if (i != j && i % j == 0)
+                    {
+                        prim = false;
+                        break;
+                    }
+                }
+                if (prim)
+                {
+                    primBroj++;
+                }
+                prim = true;
+
+            }
+
+            Console.WriteLine(primBroj);
+            Console.ReadKey();
+        }
+    }
+}
